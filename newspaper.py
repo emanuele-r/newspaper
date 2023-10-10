78e3b0620fbfd15dd12ef7f5dbd08c38c68c1ca6
Emanuele Rossi <109061971+emanuele-r@users.noreply.github.com>
2023-10-10 19:55:05 +0200

    Update newspaper.py

--- newspaper.py
+++ newspaper.py
@@ -3,6 +3,7 @@ import requests
 import plotly.express as px
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import emoji
 
 # Set Streamlit page configuration
 st.set_page_config(page_title="Keyword news search", page_icon=":newspaper:")
@@ -19,11 +20,7 @@ def load_search_history():
 
 search_history = load_search_history()
 
-
-
 def search_news(keyword: str):
-  
-
     response = requests.get(f"https://newsapi.org/v2/everything?q={keyword}&apiKey=89de75b718bb45ba884f256d3b1710cc")
 
     articles = []
@@ -42,8 +39,6 @@ sia = SentimentIntensityAnalyzer()
 
 input_data = st.text_input("Enter keywords")
 
-
-
 if input_data:
     # Add the current search query to the search history
     search_history.append(input_data)
@@ -58,24 +53,49 @@ if input_data:
     # Display the number of articles found
     st.info(f"Found {len(articles)} articles")
 
-    # Display each article with title, author, link, extracted topics, and sentiment analysis
+    # Initialize counters for sentiment analysis
+    positive_count = 0
+    negative_count = 0
+    neutral_count = 0
+
+    # Display each article with title, author, link, and sentiment analysis
     for index, article in enumerate(articles):
         with st.expander(f"Article {index + 1} - {article.get('title', 'No title available')}"):
             title = article.get('title', 'No title available')
             author = article.get('author', 'No author available')
             url = article.get('url', '#')
             content = article.get('content', '')
-            
-           
-            
+
             # Perform sentiment analysis
             sentiment_scores = sia.polarity_scores(content)
             sentiment = "Positive" if sentiment_scores['compound'] > 0 else "Negative" if sentiment_scores['compound'] < 0 else "Neutral"
             
+            # Update sentiment counters
+            if sentiment == "Positive":
+                positive_count += 1
+            elif sentiment == "Negative":
+                negative_count += 1
+            else:
+                neutral_count += 1
+            
+            # Define emojis for sentiment
+            sentiment_emoji = "😃" if sentiment == "Positive" else "😞" if sentiment == "Negative" else "😐"
+
             st.write(f"Title: {title}")
             st.write(f"Author: {author}")
             st.write(f"Link: [Read More]({url})")
-            st.write(f"Sentiment: {sentiment}")
+            st.write(f"Sentiment: {sentiment} {sentiment_emoji}")
+
+    # Create a bar chart to visualize sentiment distribution
+    sentiment_data = {
+        "Sentiment": ["Positive", "Negative", "Neutral"],
+        "Count": [positive_count, negative_count, neutral_count]
+    }
+    sentiment_df = pd.DataFrame(sentiment_data)
+
+    fig_sentiment = px.bar(sentiment_df, x="Sentiment", y="Count", color="Sentiment",
+                           labels={'Count': 'Number of Articles'}, title='Sentiment Analysis Distribution')
+    st.plotly_chart(fig_sentiment, use_container_width=True)
 
 # Data Visualization
 if input_data and articles:
