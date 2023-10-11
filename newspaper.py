bee833d939e7f7ad76d8db6ec178a391a2683765
Emanuele Rossi <109061971+emanuele-r@users.noreply.github.com>
2023-10-11 14:07:01 +0200

    Update newspaper.py

--- newspaper.py
+++ newspaper.py
@@ -104,6 +104,7 @@ def display_articles(articles):
 
     return positive_count, negative_count, neutral_count, article_data
 
+
 def display_topics_and_analytics(articles, article_data):
     st.subheader("Topics Tags")
     
@@ -122,7 +123,7 @@ def display_topics_and_analytics(articles, article_data):
     
     st.subheader("Data Analytics")
 
-    if article_data and article_data[0] and "Sentiment" in article_data[0]:
+    if article_data and any("Sentiment" in article for article in article_data):
         # Create a pandas DataFrame from the article data
         df = pd.DataFrame(article_data)
         st.dataframe(df)
@@ -132,6 +133,9 @@ def display_topics_and_analytics(articles, article_data):
         st.bar_chart(sentiment_counts)
     else:
         st.info("No sentiment data available for analytics.")
+
+
+
 
 # Main function
 def main():
